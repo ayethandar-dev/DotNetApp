74a69fad3a45866a79d9a18284eca5893a949d57
atd-ayethandar <atd.ayethandar127@gmail.com>
2026-01-12 22:33:28 +0900

    Window Auth

--- DotNetApp1/Controllers/HomeController.cs
+++ DotNetApp1/Controllers/HomeController.cs
@@ -9,15 +9,33 @@ namespace DotNetApp1.Controllers
 {
     public class HomeController : Controller
     {
+        protected string CurrentUser
+        {
+            get
+            {
+                if (User.Identity.IsAuthenticated)
+                    return User.Identity.Name;
+
+#if DEBUG
+                return "TEST\\developer01";
+#else
+            return null;
+#endif
+            }
+        }
         // GET: Home
         public ActionResult Index()
         {
-            using (var con = new SqlConnection(Utils.DBHelper.ConnectionString))
-            {
-                con.Open();
-            }
+            return Content(
+                $"IsAuthenticated: {User.Identity.IsAuthenticated}\n" +
+                $"Name: {User.Identity.Name}"
+            );
+            //using (var con = new SqlConnection(Utils.DBHelper.ConnectionString))
+            //{
+            //    con.Open();
+            //}
 
-            return Content("DB Connected Successfully");
+            //return Content("DB Connected Successfully");
         }
     }
 }
